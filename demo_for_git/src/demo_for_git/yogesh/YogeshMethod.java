d162eca0c3d56cd610b75a352387c1822080990d
Sagar <Sagar@DESKTOP-5I5MFGJ>
2024-09-28 12:11:37 +0530

    Yogesh Code

---
+++ demo_for_git/src/demo_for_git/yogesh/YogeshMethod.java
@@ -0,0 +1,25 @@
+package demo_for_git.yogesh;
+
+public class YogeshMethod {
+	
+		
+		public void m1() {
+			System.out.println("Method 1");
+		}
+		
+		public void m2() {
+			System.out.println("Method 2");
+		}
+		
+		
+		public void m3() {
+			System.out.println("Method 3");
+		}
+		
+		public static void main(String[] args) {
+			
+			System.out.println("Yogesg Programe");
+			
+		}
+
+}
